d52d4c33cd09da73bb57575d7b58ce41fe22d5c8
YukiIshikawa <ishikawayuki0304@gmail.com>
2021-02-05 14:51:49 +0000

    [finish]3-6

--- sample_app/db/migrate/20210205114140_create_lists.rb
+++ sample_app/db/migrate/20210205114140_create_lists.rb
@@ -2,8 +2,8 @@ class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
       t.string :title
-      t.string :bo
+      t.string :body
       t.timestamps
-    endrails 
+    end
   end
 end
