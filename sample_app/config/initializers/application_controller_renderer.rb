5066607a5b32551a49a0327c5857081094387bc9
YukiIshikawa <ishikawayuki0304@gmail.com>
2021-02-06 06:06:43 +0000

    [finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8c39af636b02831dda8d9ee3d71912693bc6cc9900acbc64cb348cde2d37f52b4b13b332efe7b3ea0a1b88f81c5b0c827651195cfa907a5916191ffd6fe30c59'
