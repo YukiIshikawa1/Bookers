de20b02f659cc0e7f19ef77468aa001f0ccd7c5b
YukiIshikawa <ishikawayuki0304@gmail.com>
2021-03-13 04:53:46 +0000

    [start]応用課題２まで

---
+++ bookers22/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root :to => "homes#top"
+  get "homes/about" => "homes#about"
+
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books,only: [:index,:show,:edit,:create,:destroy,:update]
+end
